1c05a027c4441fd95549b6c6936e460ad10fd906
ANX <danielgrischaeff@yandex.ru>
2025-07-15 00:14:10 +0800

    Updated index.js. Updated login, register, logout

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const saltrate = 10;
 
+
 const tgbot = require('node-telegram-bot-api');
 const mysql = require('mysql2');
 
@@ -14,68 +15,126 @@ const pool = mysql.createPool({
 	password: ""
 });
 
-var keyboards;
+const { promisify } = require('util');
+const queryAsync = promisify(pool.query).bind(pool);
+
+const logged_in_users = {};
+const admins = [1292731256];
+
+var keyboards = {
+	main_menu: {
+		reply_markup: {
+			keyboard: [ ["Создать обращение"], ["Список обращений"], ["Выйти из системы"] ]
+		}
+	},
+	retry_menu: {
+		reply_markup: {
+			keyboard: [ ["Зарегистрироваться"], ["Попытаться еще раз"] ]
+		}
+	},
+	start_menu: {
+		reply_markup: {
+			keyboard: [ ["Зарегистрироваться"], ["Войти"] ]
+		}
+	},
+};
+
+/*bot.onText(/\/start/i, async (msg) => {
+	const rows = await queryAsync("SELECT * FROM users WHERE chatId=?", [msg.chat.id]);
+	if (rows.length === 0) return bot.sendMessage(msg.chat.id, "Здравствуйте! Вас приветствует бот Клиентская рассылка. Вы не зарегистрированы. Пожалуйста, выберите нужный пункт меню.",
+	keyboards.reg_menu);
+	const userData = rows[0];
+	logged_in_users[msg.chat.id] = userData.id;
+	if (userData.isOnline === 0) return bot.sendMessage(msg.chat.id, `Здравствуйте, ${userData.name[0]}. ${userData.patronymic[0]}. ${userData.surname}! Вы зарегистрированы, но не вошли в систему.`,
+	keyboards.login_menu);
+	bot.sendMessage(msg.chat.id, `Здравствуйте, ${userData.name[0]}. ${userData.patronymic[0]}. ${userData.surname}! Выберите нужный пункт меню.`,
+	keyboards.main_menu);
+});*/
 
 bot.onText(/\/start/i, async (msg) => {
-	await bot.sendMessage(msg.chat.id, "Здравствуйте! Вас приветствует бот Клиентская рассылка. Пожалуйста, выберите нужный пункт меню.", {
-		reply_markup: {
-			keyboard: [ ["Найти меня по ID"], ["Зарегистрироваться"] ],
-		},
-	});
+	const rows = await queryAsync("SELECT * FROM users WHERE chatId=?", [msg.chat.id]);
+	if (rows.length === 0) return bot.sendMessage(msg.chat.id, "Здравствуйте! Вас приветствует бот Клиентская рассылка. Пожалуйста, выберите нужный пункт меню.",
+	keyboards.start_menu);
+	const userData = rows[0];
+	logged_in_users[msg.chat.id] = userData.id;
+	if (userData.isOnline === 0) return bot.sendMessage(msg.chat.id, `Здравствуйте, ${userData.name[0]}. ${userData.patronymic[0]}. ${userData.surname}! Вы зарегистрированы, но не вошли в систему.`,
+	keyboards.start_menu);
+	bot.sendMessage(msg.chat.id, `Здравствуйте, ${userData.name[0]}. ${userData.patronymic[0]}. ${userData.surname}! Выберите нужный пункт меню.`,
+	keyboards.main_menu);
 });
 
 const finduserhandle = async (msg) => {
-	const id = await bot.sendMessage(msg.chat.id, "Напишите, пожалуйста, ваш ID", {
-		reply_markup: {
-			force_reply: true,
-		},
-	});
-	bot.onReplyToMessage(msg.chat.id, id.message_id, async (idMsg) => {
-		const id = idMsg.text;
-		pool.query("SELECT * FROM users WHERE id=?", [id], async (err, data) => {
-			if (err) return bot.sendMessage(msg.chat.id, "Возникла ошибка во время поиска. Пожалуйста, проверьте правильность ввода данных.");
-			if (data.length === 0) return bot.sendMessage(msg.chat.id, "Такого пользователя не существует. Желаете ли зарегистрироваться или попытаться еще раз?", {
-				reply_markup: {
-					keyboard: [ ["Зарегистрироваться"], ["Попытаться еще раз"] ],
-					one_time_keyboard: true,
-				},
-			});
-			bot.sendMessage(msg.chat.id, "Пользователь обнаружен.");
+	const rows = await queryAsync("SELECT * FROM users WHERE chatId=?", [msg.chat.id]);
+	const userData = rows[0];
+	if (rows.length > 0 && userData.isOnline === 0) {		
+		const pswrdenter = await bot.sendMessage(msg.chat.id, "Введите пароль.", {
+			reply_markup: {
+				force_reply: true,
+			},
+		});
+		bot.onReplyToMessage(msg.chat.id, pswrdenter.message_id, async (pswrdMsg) => {
+			const pswrd = pswrdMsg.text;
+			const hashedpswrd = userData.password;
 			
-			const userRecord = data[0];
-			
-			const pswrdenter = await bot.sendMessage(msg.chat.id, "Введите пароль.", {
-				reply_markup: {
-					force_reply: true,
-				},
+			bcrypt.compare(pswrd, hashedpswrd, function (err, res) {
+				if (err) {
+					bot.sendMessage(admins[0], "User " + userData.id + " encountered an issue during login.\nMsg chat id: "
+					+ msg.chat.id + "\nErr msg: " + err);
+					return bot.sendMessage(msg.chat.id, "Возникла ошибка. Попробуйте позже.");
+				};
+				if (!res) return bot.sendMessage(msg.chat.id, "Неверный пароль.");
+				logged_in_users[msg.chat.id] = userData.id;
+				queryAsync("UPDATE users SET isOnline=1 WHERE id=?", [userData.id]);
+				bot.sendMessage(msg.chat.id, `Добро пожаловать в систему, ${userData.name[0]}. ${userData.patronymic[0]}. ${userData.surname}.`,
+				keyboards.main_menu);
 			});
-			bot.onReplyToMessage(msg.chat.id, pswrdenter.message_id, async (pswrdMsg) => {
-				const pswrd = pswrdMsg.text;
-				const hashedpswrd = userRecord.password;
+		});
+	} else {
+		const id = await bot.sendMessage(msg.chat.id, "Напишите, пожалуйста, ваш ID", {
+			reply_markup: {
+				force_reply: true,
+			},
+		});
+		bot.onReplyToMessage(msg.chat.id, id.message_id, async (idMsg) => {
+			const id = idMsg.text;
+			pool.query("SELECT * FROM users WHERE id=?", [id], async (err, data) => {
+				if (err) return bot.sendMessage(msg.chat.id, "Возникла ошибка во время поиска. Пожалуйста, проверьте правильность ввода данных.");
+				if (data.length === 0) return bot.sendMessage(msg.chat.id, "Такого пользователя не существует."
+				+ " Желаете ли зарегистрироваться или попытаться еще раз?",
+				keyboards.retry_menu);
+				bot.sendMessage(msg.chat.id, "Пользователь обнаружен.");
 				
-				console.log(pswrd);
-				console.log(hashedpswrd);
-				bcrypt.compare(pswrd, hashedpswrd, function (err, res) {
-					if (err) {
-						console.error(err);
-						return bot.sendMessage(msg.chat.id, "Возникла ошибка. Попробуйте позже.");
-					};
-					if (!res) return bot.sendMessage(msg.chat.id, "Неверный пароль.");
-					bot.sendMessage(msg.chat.id, `Добро пожаловать в систему, ${userRecord.name[0]}. ${userRecord.patronymic[0]}. ${userRecord.surname}.`);
+				const userData = data[0];
+				
+				const pswrdenter = await bot.sendMessage(msg.chat.id, "Введите пароль.", {
+					reply_markup: {
+						force_reply: true,
+					},
+				});
+				bot.onReplyToMessage(msg.chat.id, pswrdenter.message_id, async (pswrdMsg) => {
+					const pswrd = pswrdMsg.text;
+					const hashedpswrd = userData.password;
+					
+					bcrypt.compare(pswrd, hashedpswrd, function (err, res) {
+						if (err) {
+							bot.sendMessage(admins[0], "User " + userData.id + " encountered an issue during login.\nMsg chat id: "
+							+ msg.chat.id + "\nErr msg: " + err);
+							return bot.sendMessage(msg.chat.id, "Возникла ошибка. Попробуйте позже.");
+						};
+						if (!res) return bot.sendMessage(msg.chat.id, "Неверный пароль.");
+						logged_in_users[msg.chat.id] = userData.id;
+						pool.query("UPDATE users SET chatId=?, isOnline=1 WHERE id=?", [msg.chat.id, userData.id]);
+						bot.sendMessage(msg.chat.id, `Добро пожаловать в систему, ${userData.name[0]}. ${userData.patronymic[0]}. ${userData.surname}.`,
+						keyboards.main_menu);
+					});
 				});
 			});
 		});
-		
-	});
+	};
 };
 
-bot.onText(/\Найти меня по ID/i, (msg) => {
-	finduserhandle(msg);
-});
-
-bot.onText(/\Попытаться еще раз/i, (msg) => {
-	finduserhandle(msg);
-});
+bot.onText(/\Войти/i, (msg) => finduserhandle(msg));
+bot.onText(/\Попытаться еще раз/i, (msg) => finduserhandle(msg));
 
 bot.onText(/\Зарегистрироваться/i, async (msg) => {
 	const reg = await bot.sendMessage(msg.chat.id, "\nУкажите, пожалуйста, ваше ФИО, дату рождения \\(ДД\\.ММ\\.ГГГГ\\) и пол \\(М/Ж\\) и пароль\\."
@@ -89,22 +148,47 @@ bot.onText(/\Зарегистрироваться/i, async (msg) => {
 		const lines = nameMsg.text.split(' ');
 		const formattedtext = lines.join('\n');
 		const array = formattedtext.split('\n').map(String);
-		const surname = array[0], name = array[1], patronymic = array[2], dob = array[3].split('.').reverse().join('-'), sex = array[4], pswrd = array[5];
-		array.forEach((el) => {
-			console.log(el);
-		});
+		const surname = array[0], name = array[1], patronymic = array[2],
+		dob = array[3].split('.').reverse().join('-'),
+		sex = array[4], pswrd = array[5];
+		
 		bcrypt.hash(pswrd, saltrate, function (err, hashedpswrd) {
 			if (err) return console.log("Error hashing password: ", err);
-			console.log(hashedpswrd);
-			pool.query("INSERT INTO users(`surname`, `name`, `patronymic`, `dob`, `sex`, `password`) VALUES (?, ?, ?, ?, ?, ?)", [surname, name, patronymic, dob, sex, hashedpswrd], function (err, data) {
+			pool.query("INSERT INTO users(`surname`, `name`, `patronymic`, `dob`, `sex`, `password`, `chatId`, `isOnline`) VALUES (?, ?, ?, ?, ?, ?, ?, 1)",
+						[surname, name, patronymic, dob, sex, hashedpswrd, msg.chat.id], function (err, data) {
 				if (err) {
-					console.log(err);
+					bot.sendMessage(admins[0], "User " + userData.id + " encountered an issue during registration.\nMsg chat id: "
+					+ msg.chat.id + "\nErr msg: " + err);
 					return bot.sendMessage(msg.chat.id, "Возникла ошибка при регистрации. Проверьте правильность ввода данных, попробуйте позже или обратитесь в тех. поддержку если ошибка остается.");
 				};
-				bot.sendMessage(msg.chat.id, `Регистрация прошла успешно! Добро пожаловать в систему, ${name[0]}. ${patronymic[0]}. ${surname}.`);
+				pool.query("UPDATE users SET isOnline=1 WHERE chatId=?", [msg.chat.id]);
+				bot.sendMessage(msg.chat.id, `Регистрация прошла успешно! Добро пожаловать в систему, ${name[0]}. ${patronymic[0]}. ${surname}.`, keyboards.main_menu);
 			});
 		});
 	});
+});
+
+bot.onText(/\Создать обращение/i, (msg) => {
+	console.log("haha");
+});
+
+bot.onText(/\Список обращений/i, (msg) => {
+	console.log("haha");
+});
+
+bot.onText(/\Выйти из системы/i, async (msg) => {
+	await queryAsync("UPDATE users SET isOnline=0 WHERE chatId=?", [msg.chat.id]);
+	delete logged_in_users[msg.chat.id];
+	await bot.sendMessage(msg.chat.id, "Вы успешно вышли из системы.", keyboards.start_menu);
+});
+
+//admin
+
+bot.onText(/\broadcast (.+)/i, async (msg, match) => {
+	if (!admins.include(msg.chat.id)) return bot.sendMessage(msg.chat.id, "Вы не являетесь администратором системы", keyboards.main_menu);
+	
+	const message = match[1];
+	console.log("haha");
 });
 
 process.on('SIGINT', async () => {
